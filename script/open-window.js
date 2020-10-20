941629a93ca9a3d46a547e4538530a5c876ccfe4
cxq <1249560518@qq.com>
2020-10-20 15:07:54 +0800

    js案例-打开新标签页

--- script/open-window.js
+++ script/open-window.js
@@ -1,5 +1,7 @@
 window.onload = () => {
+    /*从DOM树种获取节点元素斌返回*/
     let openWindowBtn = document.querySelector("#open-window");
+    /*为元素挂载点击事件*/
     openWindowBtn.onclick = () => {
         let newWindow = window.open("about:blank", "_blank");
         newWindow.document.write("abc");
