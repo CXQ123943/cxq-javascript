e6e9febd37c32b5e9646e0c6d57995191b64dff1
cxq <1249560518@qq.com>
2020-10-20 16:46:51 +0800

    js案例-页面弹窗

---
+++ script/page-popup.js
@@ -0,0 +1,13 @@
+function myAlert() {
+    alert("清理完成")
+}
+
+function myConfirm() {
+    let result = confirm("确定");
+    console.log(result);
+}
+
+function myPrompt() {
+    let result = prompt("请输入“删除”来删除内容");
+    console.log(result);
+}
